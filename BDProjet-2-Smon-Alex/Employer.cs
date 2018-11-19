d24841fd9740413eae134c0cb3294fcf214b37d3
AlexanderPorras <45177397+AlexanderPorras@users.noreply.github.com>
2018-11-19 12:28:52 -0500

    Inititial commit

---
+++ BDProjet-2-Smon-Alex/Employer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BDProjet_2_Smon_Alex
+{
+    class Employer
+    {
+        public int noEmployer { get; set; }
+        public string motDePasseEmployer { get; set; }
+        public string nomEmployer { get; set; }
+        public string prenomEmployer { get; set; }
+        public char sexeEmployer { get; set; }
+        public int ageEmployer { get; set; }
+        public int noCiviqueEmployer { get; set; }
+        public string rueEmployer { get; set; }
+        public string villeEmployer { get; set; }
+        public string idProvienceEmployer { get; set; }
+        public string codePostalEmployer { get; set; }
+        public string telephoneEmployer { get; set; }
+        public string cellulaireEmployer { get; set; }
+        public string courrielEmployer { get; set; }
+        public decimal saliereHoraireEmployer { get; set; }
+        public int notypeEmployeEmployer { get; set; }
+        public string remarqueEmployer { get; set; }
+    }
+}
